866e66982587278619664ae5a2e921a381531cbe
errajibadr <errajibadr95@gmail.com>
2024-09-15 22:03:28 +0200

    test

--- js/chatWidget_1.js
+++ js/chatWidget_1.js
@@ -2,5 +2,5 @@
 import { RemoteRunnable } from 'https://esm.sh/@langchain/core@0.3.x/runnables/remote';
 
 (function() {
-  
+  console.log('chatWidget_1.js');
 })();
--- js/chatWidget_2.js
+++ js/chatWidget_2.js
@@ -1,4 +1,4 @@
 
 (function() {
-  
+  console.log('chatWidget_2.js');
 })();
